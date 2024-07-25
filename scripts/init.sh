7d78e949471d5b7fbf9890e0a51bb35ca4e39077
Nate H <nateth@gmail.com>
2024-07-24 22:45:28 -0400

    Updating some documentation typos and removing unused classes.

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,13 +13,12 @@ initRpiKvmTmux()
 	tmux select-pane -t rpi-kvm:kvm.3
     # tmux split-window -v -t rpi-kvm
 
-    tmux send-keys -t rpi-kvm:kvm.0 'cd $RPI_KVM_PATH && reset && sudo ./rpi_kvm/kvm_service.py' C-m
-    tmux send-keys -t rpi-kvm:kvm.1 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/web.py' C-m
-    # tmux send-keys -t rpi-kvm:kvm.2 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/info_hub.py' C-m
-    # tmux send-keys -t rpi-kvm:kvm.3 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/mouse.py' C-m
-    tmux send-keys -t rpi-kvm:kvm.2 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/mouse.py' C-m
-    # tmux send-keys -t rpi-kvm:kvm.4 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/keyboard.py' C-m
-    tmux send-keys -t rpi-kvm:kvm.3 'cd $RPI_KVM_PATH && reset && ./rpi_kvm/keyboard.py' C-m
+    tmux send-keys -t rpi-kvm:kvm.0 "cd $RPI_KVM_PATH && reset && sudo ./rpi_kvm/kvm_service.py" C-m
+    tmux send-keys -t rpi-kvm:kvm.1 "cd $RPI_KVM_PATH && reset && ./rpi_kvm/web.py" C-m
+    # tmux send-keys -t rpi-kvm:kvm.3 "cd $RPI_KVM_PATH && reset && ./rpi_kvm/mouse.py" C-m
+    tmux send-keys -t rpi-kvm:kvm.2 "cd $RPI_KVM_PATH && reset && ./rpi_kvm/mouse.py" C-m
+    # tmux send-keys -t rpi-kvm:kvm.4 "cd $RPI_KVM_PATH && reset && ./rpi_kvm/keyboard.py" C-m
+    tmux send-keys -t rpi-kvm:kvm.3 "cd $RPI_KVM_PATH && reset && ./rpi_kvm/keyboard.py" C-m
 
 	tmux select-pane -t rpi-kvm:kvm.0
 }
