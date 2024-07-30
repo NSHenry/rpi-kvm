c25861cb5425a436c8aaca1f8911f19cc8bdbe41
Nate H <nateth@gmail.com>
2024-07-30 08:27:12 -0400

    chore: Update rpi-kvm.sh and rpi-kvm-dev.sh scripts

--- rpi-kvm.sh
+++ rpi-kvm.sh
@@ -9,6 +9,9 @@ case "$1" in
         ;;
     debug)
         sudo ./rpi-kvm.sh restart
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led0/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led1/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led2/brightness'
         sudo ./rpi-kvm.sh attach
         ;;
     attach)
@@ -16,18 +19,30 @@ case "$1" in
         ;;
     kill)
         sudo tmux kill-session -t rpi-kvm
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led0/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led1/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led2/brightness'
         ;;
     stop)
         sudo tmux kill-session -t rpi-kvm
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led0/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led1/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led2/brightness'
         ;;
     quit)
         sudo tmux kill-session -t rpi-kvm
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led0/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led1/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led2/brightness'
         ;;
     start)
         sudo ./scripts/init.sh
         ;;
     restart)
         sudo tmux kill-session -t rpi-kvm
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led0/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led1/brightness'
+        sudo sh -c 'echo 0 > /sys/class/leds/usr_led2/brightness'
         sudo ./scripts/init.sh
         ;;
     update-mac)
